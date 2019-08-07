3e31ac718f13f4da5dc04ebd276037a466159844
tamannasiddiqui <tamannasiddiqui110@gmail.com>
2019-08-08 03:13:08 +0530

    work in progress , Part C(post)

--- js/post.js
+++ js/post.js
@@ -2,5 +2,28 @@ function editPost()
 {
     document.getElementById('editButton').innerHTML = "Save "+'<i class="fa fa-save"></i>';
     document.getElementById('blogBody').contentEditable = "true";
+    document.getElementById('blogTitleNew').contentEditable = "true";
 
+}
+
+function commentDisplay(id)
+{
+   var addComment = id.value;
+   if(addComment=="")
+   {
+       alert("Enter comments!");
+   }
+
+   else
+   {
+       document.getElementById('addComments').innerHTML +='<p>'+addComment+'</p>';
+   }
+
+}
+
+function addComment(id){
+    var addEventName = id.value +'<br>';
+      var a = document.getElementById('userComments');
+      document.getElementById('addEventNames').innerHTML += '<p>'+addEventName+'</p>';          
+      a.value=a.defaultValue;
 }
