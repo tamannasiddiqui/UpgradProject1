d4da38eeaed0c2ab51a300d7bbb669c2b54ba9b0
tamannasiddiqui <tamannasiddiqui110@gmail.com>
2019-08-07 21:01:18 +0530

    commit 9, changes in bloglist

--- js/header.js
+++ js/header.js
@@ -6,30 +6,30 @@ function validateForm2(){
 
 
     
-    if( fullname == "" ) {
-        alert( "Please provide your fullname!" );
-        fullname.focus() ;
-        return false;
-     }
+    // if( fullname == "" ) {
+    //     alert( "Please provide your fullname!" );
+    //     fullname.focus() ;
+    //     return false;
+    //  }
 
-    if( username == "" ) {
-        alert( "Please provide your username!" );
-        username.focus() ;
-        return false;
-    }
+    // if( username == "" ) {
+    //     alert( "Please provide your username!" );
+    //     username.focus() ;
+    //     return false;
+    // }
 
     
-     if( password == "" ) {
-        alert( "Please provide your password!" );
-        password.focus() ;
-        return false;
-    }
+    //  if( password == "" ) {
+    //     alert( "Please provide your password!" );
+    //     password.focus() ;
+    //     return false;
+    // }
     
-     if( confirmPassword == "" ) {
-        alert( "Please confirm your password!" );
-        confirmPassword.focus() ;
-        return false;
-    }
+    //  if( confirmPassword == "" ) {
+    //     alert( "Please confirm your password!" );
+    //     confirmPassword.focus() ;
+    //     return false;
+    // }
 
 
     if (password != confirmPassword) {
@@ -46,21 +46,11 @@ function validateForm1(){
     var userName = document.getElementById('username1').value;
     var password = document.getElementById("password1").value;
 
-    if( userName == "" ) {
-        alert( "Please provide your name!" );
-        userName.focus() ;
-        return false;
-     }
-
-     if( password == "" ) {
-        alert( "Please provide your password!" );
-        password.focus() ;
-        return false;
-     }
-     
+     userName.reset();
+     password.reset();
 }
 
-function resetForm1(){
-    document.getElementById("signInForm").reset();
+// function resetForm1(){
+//     document.getElementById("signInForm").reset();
     
-}
+// }
