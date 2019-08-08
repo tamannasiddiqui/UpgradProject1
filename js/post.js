b6d797bbc588f81cd6165f138e7ff615377fe07b
tamannasiddiqui <tamannasiddiqui110@gmail.com>
2019-08-08 15:52:36 +0530

    bloglist module made responsive

--- js/post.js
+++ js/post.js
@@ -1,14 +1,18 @@
 function editPost()
 {
     document.getElementById('editButton').innerHTML = "Save "+'<i class="fa fa-save"></i>';
+
     document.getElementById('blogBody').contentEditable = "true";
+    document.getElementById('blogBody').style="border"
+
     document.getElementById('blogTitleNew').contentEditable = "true";
 
 }
 
 function commentDisplay(id)
 {
-   var addComment = id.value;
+   var addComment = id.value +'<br>';
+   var a = document.getElementById('userComments');
    if(addComment=="")
    {
        alert("Enter comments!");
@@ -17,13 +21,14 @@ function commentDisplay(id)
    else
    {
        document.getElementById('addComments').innerHTML +='<p>'+addComment+'</p>';
+       a.value=a.defaultValue;
    }
 
 }
 
-function addComment(id){
-    var addEventName = id.value +'<br>';
-      var a = document.getElementById('userComments');
-      document.getElementById('addEventNames').innerHTML += '<p>'+addEventName+'</p>';          
-      a.value=a.defaultValue;
-}
+// function addComment(id){
+//     var addEventName = id.value +'<br>';
+//       var a = document.getElementById('userComments');
+//       document.getElementById('addEventNames').innerHTML += '<p>'+addEventName+'</p>';          
+//       a.value=a.defaultValue;
+// }
